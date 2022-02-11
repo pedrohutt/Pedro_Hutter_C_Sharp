011477dbaf0ec331668a2e69bf8b5ddc53f76688
pedro <pedro@DESKTOP-RPEIODU>
2022-02-11 01:31:25 -0300

    Add project files.

---
+++ Subtração/Sub.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    public class Sub
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Digite o primeiro número: ");
+            var first = Console.ReadLine();
+            if (!int.TryParse(first, out var a))
+                Console.WriteLine("Número inválido");
+
+            Console.WriteLine("Digite o segundo número: ");
+            var second = Console.ReadLine();
+            if (!int.TryParse(second, out var b))
+                Console.WriteLine("Número inválido");
+
+            int c = a - b;
+            Console.WriteLine($"O resultado da subtração de {a} menos {b} é igual a: {c}");
+        }
+
+    }
+}
