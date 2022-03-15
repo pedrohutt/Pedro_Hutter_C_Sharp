cfcfe1a51e1fd58738e552e2a0259856e4288e47
Pedro Paulo Barillo Taboada Hutter <pedro.hutter@al.infnet.edu.br>
2022-03-15 18:40:43 -0300

    up

--- TP3/ConsoleApp/Console/Program.cs
+++ TP3/ConsoleApp/Console/Program.cs
@@ -1,7 +1,7 @@
-﻿using Dominio;
-using Infraestrutura;
+﻿using TeamDomain;
 using System;
 using System.Globalization;
+using Infraestructure;
 
 namespace ConsoleApp
 {
@@ -12,9 +12,7 @@ namespace ConsoleApp
         {
             MenuOpcoes();
             Console.ReadKey();
-
         }
-
         public static void MenuOpcoes()
         {
             bool Menu = true;
@@ -22,12 +20,12 @@ namespace ConsoleApp
             do
             {
                 Console.WriteLine("\n### Gerenciador de Times de Futebol ###"
-                    + "\n Escolha uma das opções..."
-                    + "\n 1 - Adicionar um novo Time"
-                    + "\n 2 - Pesquise por um Time (Nome)"
-                    + "\n 3 - Para fechar o Programa");
-                opcaoMenu = Console.ReadLine();
+                                + "\n Escolha uma das opções..."
+                                + "\n 1 - Adicionar um novo Time"
+                                + "\n 2 - Pesquise por um Time (Nome)"
+                                + "\n 3 - Para fechar o Programa");
 
+                opcaoMenu = Console.ReadLine();
                 switch (opcaoMenu)
                 {
                     case "1":
@@ -64,7 +62,6 @@ namespace ConsoleApp
             if (!int.TryParse(Console.ReadLine(), out titulosES))
                 Console.WriteLine("Número inválido");
 
-
             Console.WriteLine("Informe a data de Criação do Time (formato dd/MM/yyyy):");
             if (!DateTime.TryParse(Console.ReadLine(), out dataCriacao))
             {
@@ -72,9 +69,14 @@ namespace ConsoleApp
                 Console.ReadKey();
             }
 
-            Console.WriteLine("Confirme os dados registrados..." +
-                "1 - Corretos" +
-                "2 - Incorretos");
+            Console.WriteLine($"\n Nome {nome}  -  Estado {estado}" +
+                              $"\n Titulos Brasileiros {titulosBR}  -  Titulos Estaduais {titulosES}" +
+                              $"\n Data de Criação {dataCriacao}");
+
+            Console.WriteLine("\nConfirme os dados registrados e escolha uma opção." +
+                "\n 1 - Corretos" +
+                "\n 2 - Incorretos");
+            Console.ReadLine();
 
         }
         public static void SearchTeam()
