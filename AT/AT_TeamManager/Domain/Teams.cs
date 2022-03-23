d3ddb9015d223794ffed10956bfeec9df9f1039d
Pedro Paulo Barillo Taboada Hutter <pedro.hutter@al.infnet.edu.br>
2022-03-23 14:49:21 -0300

    AT_UP

---
+++ AT/AT_TeamManager/Domain/Teams.cs
@@ -0,0 +1,19 @@
+﻿namespace Domain
+{
+    public class Teams
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string? Nome { get; set; }
+        public string? Estado { get; set; }
+        public int TitulosBrasileiros { get; set; }
+        public int TitulosEstaduais { get; set; }
+        public DateTime DataCriacao { get; set; }
+
+        public int TempoAtivo()
+        {
+            int now = DateTime.Now.Year;
+            int dataCriacao = now - DataCriacao.Year;
+            return dataCriacao;
+        }
+    }
+}
---
+++ AT/AT_TeamManager/TeamsRepositorie/TeamsRepositorie.cs
@@ -0,0 +1,37 @@
+﻿using Domain;
+
+namespace Repositorie
+{
+    public class TeamsRepositorie
+    {
+        private static List<Teams> ListaTeams = new List<Teams>();
+
+        public static void RegisterTeam(Teams team)
+        {
+            if (team != null)
+            {
+                ListaTeams.Add(team);
+            }
+        }
+        public static List<Teams> SearchTeam(string searchString)
+        {
+            List<Teams> searchResult = new List<Teams>();
+            foreach (var team in ListaTeams)
+            {
+                if (team.Nome.Contains(searchString))
+                {
+                    searchResult.Add(team);
+                }
+            }
+            return searchResult;
+        }
+        public static string ShowTeamInfo(Teams team)
+        {
+            string teamInfo = $"\nNome do Time: {team.Nome} - Estado: {team.Estado}" +
+                $"\n Titulos Estaduais {team.TitulosEstaduais} - Titulos Brasileiros {team.TitulosBrasileiros}" +
+                $"\n O Clube já possui {team.TempoAtivo()} anos de história!\n" +
+                $"\n O Código Guid do Time é {team.Id}";
+            return teamInfo;
+        }
+    }
+}
